e131fc5420c473917e5542fc2629c01398e94243
Tyyagoo <46688275+Tyyagoo@users.noreply.github.com>
2021-07-21 18:18:10 -0300

    (Hyperskill) SmartCalculator - Stage 1/8

--- Courses/Hyperskill/Java Developer/Hard/SmartCalculator/Smart Calculator/task/src/calculator/Main.java
+++ Courses/Hyperskill/Java Developer/Hard/SmartCalculator/Smart Calculator/task/src/calculator/Main.java
@@ -6,6 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // put your code here
+        System.out.println(scanner.nextInt() + scanner.nextInt());
     }
 }
