a785d633a0add2dd69bfa683926d3e25782deec2
Tyyagoo <46688275+Tyyagoo@users.noreply.github.com>
2021-07-21 18:20:53 -0300

    (Hyperskill) SmartCalculator - Stage 3/8

--- Courses/Hyperskill/Java Developer/Hard/SmartCalculator/Smart Calculator/task/src/calculator/Main.java
+++ Courses/Hyperskill/Java Developer/Hard/SmartCalculator/Smart Calculator/task/src/calculator/Main.java
@@ -6,6 +6,17 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(scanner.nextInt() + scanner.nextInt());
+
+        while (true) {
+            String inputLine = scanner.nextLine();
+            if (inputLine.equals("/exit")) break;
+            if (inputLine.length() == 0) continue;
+            int sum = 0;
+            for(String n: inputLine.split(" ")) {
+                sum += Integer.parseInt(n);
+            }
+            System.out.println(sum);
+        }
+        System.out.println("Bye!");
     }
 }
