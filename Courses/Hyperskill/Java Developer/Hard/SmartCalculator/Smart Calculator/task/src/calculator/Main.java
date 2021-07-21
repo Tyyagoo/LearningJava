7d1d55f14735c97313c01c743b62a53efbfe95e5
Tyyagoo <46688275+Tyyagoo@users.noreply.github.com>
2021-07-21 18:16:07 -0300

    (Hyperskill) SmartCalculator - 0/8

---
+++ Courses/Hyperskill/Java Developer/Hard/SmartCalculator/Smart Calculator/task/src/calculator/Main.java
@@ -0,0 +1,11 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // put your code here
+    }
+}
