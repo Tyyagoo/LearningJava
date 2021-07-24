54ceac3558793cd117cc4626536fa74022d77add
Tyyagoo <46688275+Tyyagoo@users.noreply.github.com>
2021-07-24 15:43:30 -0300

    (Hyperskill) JSON DB - Stage 2/6

--- Courses/Hyperskill/Java Developer/Hard/JSON-Database/JSON Database/task/src/client/Main.java
+++ Courses/Hyperskill/Java Developer/Hard/JSON-Database/JSON Database/task/src/client/Main.java
@@ -1,8 +1,28 @@
 package client;
 
+import java.io.Console;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class Main {
+    public static final String address = "127.0.0.1";
+    public static final int port = 8080;
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Client started!");
+        try (Socket socket = new Socket(InetAddress.getByName(address), port);
+             DataInputStream input = new DataInputStream(socket.getInputStream());
+             DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
+            String content = "Give me a record # 12";
+            output.writeUTF(content);
+            System.out.printf("Sent: %s%n", content);
+            String received = input.readUTF();
+            System.out.printf("Received: %s%n", received);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Courses/Hyperskill/Java Developer/Hard/JSON-Database/JSON Database/task/src/server/Main.java
+++ Courses/Hyperskill/Java Developer/Hard/JSON-Database/JSON Database/task/src/server/Main.java
@@ -4,19 +4,52 @@ import server.console.Console;
 import server.console.UserInterface;
 import server.database.Database;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
+    public static final Pattern integerPattern = Pattern.compile("\\d+");
 
     public static void main(String[] args) {
-        Database database = new Database();
-        UserInterface.setDatabase(database);
+        try (ServerSocket serverSocket = new ServerSocket(8080)) {
+            System.out.println("Server started!");
+            try (Socket socket = serverSocket.accept();
+                 DataInputStream input = new DataInputStream(socket.getInputStream());
+                 DataOutputStream output  = new DataOutputStream(socket.getOutputStream())) {
 
-        while (UserInterface.isRunning()) {
-            try {
-                UserInterface.invoke();
-            } catch (Exception e) {
-                Console.println(e.getMessage());
+                String received = input.readUTF();
+                Console.print("Received: %s", received);
+                Matcher matcher = integerPattern.matcher(received);
+                if (matcher.find()) {
+                    int number = Integer.parseInt(matcher.group());
+                    String response = String.format("A record # %d was sent!", number);
+                    output.writeUTF(response);
+                    Console.print("Sent: %s", response);
+                }
             }
-
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+
+        /*
+        .
+            Database database = new Database();
+            UserInterface.setDatabase(database);
+
+            while (UserInterface.isRunning()) {
+                try {
+                    UserInterface.invoke();
+                } catch (Exception e) {
+                    Console.println(e.getMessage());
+                }
+
+            }
+        */
     }
 }
