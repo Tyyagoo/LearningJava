ae36db42d5cb60839150cffe5dd8cd8902a7c70e
Tyyagoo <46688275+Tyyagoo@users.noreply.github.com>
2021-07-28 14:28:41 -0300

    bkp

--- Courses/Hyperskill/Java Developer/Hard/MazeRunner/Maze Runner/task/src/maze/Main.java
+++ Courses/Hyperskill/Java Developer/Hard/MazeRunner/Maze Runner/task/src/maze/Main.java
@@ -1,41 +1,16 @@
 package maze;
 
+import java.util.*;
+
 public class Main {
 
-    enum Cell{
-        EMPTY(' '),
-        WALL('\u2588');
-
-        private final char graphic;
-
-        Cell(char graphic) {
-            this.graphic = graphic;
-        }
-
-        public void draw() {
-            System.out.printf("%s%s", graphic, graphic);
-        }
-    }
-
     public static void main(String[] args) {
-
-        Cell[][] maze = {
-                {Cell.WALL,Cell.WALL,Cell.WALL,Cell.WALL,Cell.WALL,Cell.WALL,Cell.WALL,Cell.WALL,Cell.WALL,Cell.WALL},
-                {Cell.EMPTY,Cell.EMPTY,Cell.WALL,Cell.EMPTY,Cell.WALL,Cell.EMPTY,Cell.WALL,Cell.EMPTY,Cell.EMPTY,Cell.WALL},
-                {Cell.WALL,Cell.EMPTY,Cell.WALL,Cell.EMPTY,Cell.EMPTY,Cell.EMPTY,Cell.WALL,Cell.EMPTY,Cell.WALL,Cell.WALL},
-                {Cell.WALL,Cell.EMPTY,Cell.EMPTY,Cell.EMPTY,Cell.WALL,Cell.WALL,Cell.WALL,Cell.EMPTY,Cell.EMPTY,Cell.EMPTY},
-                {Cell.WALL,Cell.EMPTY,Cell.WALL,Cell.EMPTY,Cell.EMPTY,Cell.EMPTY,Cell.EMPTY,Cell.EMPTY,Cell.WALL,Cell.WALL},
-                {Cell.WALL,Cell.EMPTY,Cell.WALL,Cell.EMPTY,Cell.WALL,Cell.EMPTY,Cell.EMPTY,Cell.EMPTY,Cell.WALL,Cell.WALL},
-                {Cell.WALL,Cell.EMPTY,Cell.WALL,Cell.EMPTY,Cell.WALL,Cell.WALL,Cell.WALL,Cell.EMPTY,Cell.WALL,Cell.WALL},
-                {Cell.WALL,Cell.EMPTY,Cell.WALL,Cell.EMPTY,Cell.EMPTY,Cell.EMPTY,Cell.WALL,Cell.EMPTY,Cell.EMPTY,Cell.WALL},
-                {Cell.WALL,Cell.EMPTY,Cell.WALL,Cell.EMPTY,Cell.EMPTY,Cell.EMPTY,Cell.WALL,Cell.EMPTY,Cell.EMPTY,Cell.WALL},
-                {Cell.WALL,Cell.WALL,Cell.WALL,Cell.WALL,Cell.WALL,Cell.WALL,Cell.WALL,Cell.WALL,Cell.WALL,Cell.WALL}};
-
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                maze[i][j].draw();
-            }
-            System.out.println();
-        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, enter the size of a maze");
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        Maze maze = new Maze(n, m);
+        maze.generate();
+        maze.draw();
     }
 }
