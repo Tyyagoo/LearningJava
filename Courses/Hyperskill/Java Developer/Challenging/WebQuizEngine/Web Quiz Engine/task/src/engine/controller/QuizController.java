56872c28a1fb1e47cc4f7e5987cb25c9cd0a95a4
Tyyagoo <46688275+Tyyagoo@users.noreply.github.com>
2021-08-06 14:15:22 -0300

    (Hyperskill) WebQuizEngine - Stage 2/6

--- Courses/Hyperskill/Java Developer/Challenging/WebQuizEngine/Web Quiz Engine/task/src/engine/controller/QuizController.java
+++ Courses/Hyperskill/Java Developer/Challenging/WebQuizEngine/Web Quiz Engine/task/src/engine/controller/QuizController.java
@@ -5,28 +5,44 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.Serializable;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 public class QuizController {
 
-    private Quiz quiz = new Quiz("The Java Logo",
-            "What is depicted on the Java logo?",
-            new String[]{"Robot", "Tea leaf", "Cup of coffee", "Bug"});
+    private final ConcurrentHashMap<Integer, Quiz> quizzesMap = new ConcurrentHashMap<>();
+    // private final CopyOnWriteArraySet<Quiz> quizzes = new CopyOnWriteArraySet<>();
 
-    @GetMapping("/api/quiz")
-    public Quiz getQuiz() {
-        return quiz;
+    @GetMapping("/api/quizzes")
+    public ResponseEntity<List<Quiz>> getAllQuizzes() {
+        return new ResponseEntity<>(List.copyOf(quizzesMap.values()), HttpStatus.OK);
     }
 
-    @PostMapping("/api/quiz")
-    public ResponseEntity<Map<String, Object>> answerQuiz(@RequestParam String answer) {
-        if ("2".equals(answer)) {
-            return new ResponseEntity<>(Map.of("success", true,
-                    "feedback", "Congratulations, you're right!"), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(Map.of("success", false,
-                    "feedback", "Wrong answer! Please, try again."), HttpStatus.OK);
-        }
+    @GetMapping("/api/quizzes/{id}")
+    public ResponseEntity<Quiz> getQuizById(@PathVariable Integer id) {
+        Optional<Quiz> quiz = Optional.ofNullable(quizzesMap.get(id));
+        return quiz.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/api/quizzes")
+    public ResponseEntity<Quiz> createQuiz(@RequestBody Quiz quiz) {
+        quiz.setId(quizzesMap.size() + 1);
+        quizzesMap.put(quiz.getId(), quiz);
+        return new ResponseEntity<>(quiz, HttpStatus.OK);
+    }
+
+    @PostMapping("/api/quizzes/{id}/solve")
+    public ResponseEntity<?> solveQuiz(@PathVariable Integer id, @RequestParam Integer answer) {
+        Optional<Quiz> quiz = Optional.ofNullable(quizzesMap.get(id));
+        return quiz.map(value -> {
+            boolean result = value.solve(answer);
+            String feedback = result ? "Congratulations, you're right!" : "Wrong answer! Please, try again.";
+            return new ResponseEntity<>(Map.of("success" , result, "feedback", feedback), HttpStatus.OK);
+        }).orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
     }
 }
--- Courses/Hyperskill/Java Developer/Challenging/WebQuizEngine/Web Quiz Engine/task/src/engine/model/Quiz.java
+++ Courses/Hyperskill/Java Developer/Challenging/WebQuizEngine/Web Quiz Engine/task/src/engine/model/Quiz.java
@@ -1,14 +1,35 @@
 package engine.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Quiz {
+    private Integer id;
     private String title;
     private String text;
     private String[] options;
 
-    public Quiz(String title, String text, String[] options) {
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Integer answer;
+
+    public Quiz(Integer id, String title, String text, String[] options, Integer answer) {
+        this.id = id;
         this.title = title;
         this.text = text;
         this.options = options;
+        this.answer = answer;
+    }
+
+    public boolean solve(Integer choice) {
+        if (answer == null || choice == null) return false;
+        return answer.equals(choice);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -33,5 +54,50 @@ public class Quiz {
 
     public void setOptions(String[] options) {
         this.options = options;
+    }
+
+    public Integer getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(Integer answer) {
+        this.answer = answer;
+    }
+
+    public static class Builder {
+        private Integer id;
+        private String title;
+        private String text;
+        private String[] options;
+        private Integer answer;
+
+        public Builder setId(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder setText(String text) {
+            this.text = text;
+            return this;
+        }
+
+        public Builder setOptions(String[] options) {
+            this.options = options;
+            return this;
+        }
+
+        public Builder setAnswer(Integer answer) {
+            this.answer = answer;
+            return this;
+        }
+
+        public Quiz build() {
+            return new Quiz(id, title, text, options, answer);
+        }
     }
 }
